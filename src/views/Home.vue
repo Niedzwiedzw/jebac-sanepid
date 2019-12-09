fe318b0501a7896799d57c7bf2f2788a0141b3a8
niedzwiedzw <niedzwiedzwo@gmail.com>
2019-12-09 10:28:08 +0100

    new title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
-    <h1>J.E.B.A.C. Sanepid</h1>
+    <h1>
+      <span class="special-char">J</span>ednolita
+      <span class="special-char">E</span>lektroniczna
+      <span class="special-char">B</span>aza
+      <span class="special-char">A</span>kronimów
+      <span class="special-char">C</span>horobowych
+      Sanepid
+    </h1>
     <jebac-sanepid-chart/>
   </div>
 </template>
@@ -22,5 +29,9 @@ export default {
     width: 100%;
     height: 100%;
     text-align: center;
+
+    .special-char {
+      color: red;
+    }
   }
 </style>
