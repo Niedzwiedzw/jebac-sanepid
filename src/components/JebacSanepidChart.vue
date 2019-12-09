2ce130c5190f91f2e72a78d7fc2830112bfe863a
niedzwiedzw <niedzwiedzwo@gmail.com>
2019-12-09 10:45:41 +0100

    better search

--- src/components/JebacSanepidChart.vue
+++ src/components/JebacSanepidChart.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="JebacSanepidChart">
-     <line-chart v-if="show" :data="chartData"/>
+    <div class="refresh-box" v-if="showAll">
+       <line-chart :data="chartData"/>
+    </div>
     <input
         class="input"
         type="text"
@@ -8,7 +10,7 @@
         aria-label="Nazwa choroby (minimum 5 znaków)"
         placeholder="Nazwa choroby (minimum 5 znaków)"
     >
-    <button @click="blink()">odśwież</button>
+    <button @click="blink">Szukaj</button>
   </div>
 </template>
 
@@ -27,9 +29,8 @@
     },
   })
   export default class JebacSanepidChart extends Vue {
+    private showAll: boolean = false;
     private search = '';
-    private _show: boolean = true;
-    private get show(): boolean { return (this.search.length > 5); }
     private data = sanepidBySicknessByDate;
     private sicknessNames: string[] = keys(first(values(this.data)));
     private get filteredKeys() { return filter(this.sicknessNames, (k: string) => includes(k, this.search)) }
@@ -38,8 +39,8 @@
       return buildMultiLineChartData(mapValues(this.data, (sicknesses) => pick(sicknesses, keys)) as any);
     }
     private blink() {
-      this._show = false;
-      setTimeout(() => this._show = true, 10);
+      this.showAll = false;
+      setTimeout(() => this.showAll = true, 10);
     }
   }
 </script>
